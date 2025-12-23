fff373c61b1a52b3085fadde0a0c82d3278c63c1
Adhish gurung <adhishgurung6@gmail.com>
2025-12-24 00:24:18 +0545

    understanding the traffic generation concept

--- traffic_generator2.c
+++ traffic_generator2.c
@@ -3,6 +3,11 @@
 #include<time.h>
 #include<unistd.h>
 
+#define ROADS 4
+#define MAX_VEHICLES 10
+#define vehicles 10
+
+typedef 
 
 int main()
 { 
@@ -10,7 +15,5 @@ int main()
   printf"======================================\n");
   printf("Starting to generating the traffics\n");
   printf"======================================\n");
- 
+  printf("Press Ctrl + C to stop the process\n");
 }
-
-
